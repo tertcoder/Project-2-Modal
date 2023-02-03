2e7c6860b74ed8af26c2fce955f8c20e0e477b90
tertcoder <82041184+tertcoder@users.noreply.github.com>
2023-02-04 00:37:24 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+// Variables
+let openBtn = document.getElementById("open-btn");
+let modalContainer = document.getElementById("modal-container");
+let closeBtn = document.getElementById("close-btn");
+
+// Event Listeners
+openBtn.addEventListener("click", () => {
+  modalContainer.style.display = "block";
+});
+
+closeBtn.addEventListener("click", () => {
+  modalContainer.style.display = "none";
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target === modalContainer) {
+    modalContainer.style.display = "none";
+  }
+});
